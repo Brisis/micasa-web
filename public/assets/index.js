7666e29f2b83dd09decc06c64717b913a24e68a5
Ben Mudzinga <benbeemudy@gmail.com>
2023-09-27 22:30:20 +0200

    image cleanup

--- public/assets/index.js
+++ public/assets/index.js
@@ -5,15 +5,6 @@ import lock from "./lock.svg";
 import logoBlack from "./logo.svg";
 import logoHome from "./logo-home.svg";
 import heroImg from "./estate.png";
-import companyLogo1 from "./company-logo-1.png";
-import companyLogo2 from "./company-logo-2.png";
-import companyLogo3 from "./company-logo-3.png";
-import companyLogo4 from "./company-logo-4.png";
-import blogImg1 from "./blog-1.png";
-import achievement from "./achievement.png";
-import avatar from "./avatar.png";
-import quotationMark from "./quotationMark.png";
-import cta from "./cta.png"
 import howItWorks from './hiw.png'
 import teamBen from './team/ben.jpg'
 import teamJane from './team/jane.jpg'
@@ -27,15 +18,6 @@ export {
   logoBlack,
   lock,
   heroImg,
-  companyLogo1,
-  companyLogo2,
-  companyLogo3,
-  companyLogo4,
-  blogImg1,
-  achievement,
-  avatar,
-  quotationMark,
-  cta,
   howItWorks,
   teamBen,
   teamVictor,
